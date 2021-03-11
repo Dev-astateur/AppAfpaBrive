3994c5c59e9450ffa04bad1d2b344e75e86382c6
Arbouz Fethi <arbouz.fethi@gmail.com>
2021-03-11 14:46:41 +0100

    Ajout d'un controller PeeController

---
+++ AppAfpaBrive.Web/Controllers/PeeController.cs
@@ -0,0 +1,28 @@
+﻿using AppAfpaBrive.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.Controllers
+{
+    public class PeeController : Controller
+    {
+        #region champ privé
+        private AFPANADbContext _dbContext = null;
+        #endregion
+        #region Constructeur
+        public PeeController(AFPANADbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
