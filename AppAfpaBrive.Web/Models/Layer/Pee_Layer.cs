7b91bf927feeccbeab09d710fe9229f3685e3d4a
louloulabeille <louloulabeille@hotmail.com>
2021-03-11 12:15:20 +0100

    ajout d'un layer pour le model Pee dans le projet

---
+++ AppAfpaBrive.Web/Models/Layer/Pee_Layer.cs
@@ -0,0 +1,19 @@
+﻿using AppAfpaBrive.BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppAfpaBrive.DAL;
+
+namespace AppAfpaBrive.Web.Models.Layout
+{
+    public class Pee_Layer
+    {
+        private readonly AFPANADbContext _dbContext = null;
+
+        public Pee_Layer (AFPANADbContext context) 
+        {
+            _dbContext = context;
+        }
+    }
+}
