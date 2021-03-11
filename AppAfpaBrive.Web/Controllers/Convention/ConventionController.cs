78d66b5deab88c3d8ec2fd74e9af3f7a4447b778
lucas Bardouillet <lucas.bardouillet@gmail.com>
2021-03-11 15:05:29 +0100

    Test Controller

---
+++ AppAfpaBrive.Web/Controllers/Convention/ConventionController.cs
@@ -0,0 +1,30 @@
+﻿using AppAfpaBrive.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.Controllers.Convention
+{
+    public class ConventionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ConventionController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(int? id)
+        {
+            ViewData["Message"] = "Your application description page " + id + " !";
+
+            return View();
+        }
+    }
+}
