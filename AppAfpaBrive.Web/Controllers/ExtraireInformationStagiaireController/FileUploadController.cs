3d3220523feceee2cbdbf5b555e7dba46ceea219
Tristan <tristanranchon24@hotmail.fr>
2021-03-11 16:07:07 +0100

    Ajout du corps de l'upload dans FileUploadController

--- AppAfpaBrive.Web/Controllers/ExtraireInformationStagiaireController/FileUploadController.cs
+++ AppAfpaBrive.Web/Controllers/ExtraireInformationStagiaireController/FileUploadController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,22 @@ namespace AppAfpaBrive.Web.Controllers
 {
     public class FileUploadController : Controller
     {
-        public IActionResult Index()
+       
+
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(IFormFile file, string path)
         {
-            return View();
+            var filePath = path;
+            if (file.Length > 0)
+            {
+                
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return Ok(new { file.Length, filePath});
         }
     }
 }
