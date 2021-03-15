8839e66244290622e50ca897ff01aaa9567ccfec
lucas Bardouillet <lucas.bardouillet@gmail.com>
2021-03-15 09:28:25 +0100

    Production layer produit formation et etablissement

---
+++ AppAfpaBrive.DAL/Layers/Layer_Code_Produit_Formation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using AppAfpaBrive.BOL;
+using System.Linq;
+
+namespace AppAfpaBrive.DAL.Layer
+{
+    public class Layer_Code_Produit_Formation
+    {
+        private readonly AFPANADbContext _db;
+        public Layer_Code_Produit_Formation(AFPANADbContext context)
+        {
+            _db = context;
+        }
+
+        public IEnumerable<ProduitFormation> Get_Etablissement_Nom(int Id)
+        {
+            return _db.ProduitFormations.Where(x=>x.CodeProduitFormation == Id);
+        }
+    }
+}
