3d2600e377c13605c49ee1a8bdeb8a2f730f0b17
Besancon Gabriel <besancon.gabriel@hotmail.fr>
2021-03-15 09:59:28 +0100

    Ajout du dossier ModelView et mise en place des controles de validation pour la création d'un nouveau produit de formation

--- AppAfpaBrive.Web/Controllers/ProduitFormation/ProduitFormationController.cs
+++ AppAfpaBrive.Web/Controllers/ProduitFormation/ProduitFormationController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppAfpaBrive.BOL;
 using AppAfpaBrive.DAL;
-using PagedList;
+
 
 
 namespace AppAfpaBrive.Web.Controllers.ProduitFormation
---
+++ AppAfpaBrive.Web/ModelView/ProduitFormationModelView.cs
@@ -0,0 +1,36 @@
+﻿using AppAfpaBrive.BOL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.ModelView
+{
+    public class ProduitFormationModelView
+    {
+        public ProduitFormationModelView()
+        {
+            OffreFormations = new HashSet<OffreFormation>();
+            ProduitFormationAppellationRomes = new HashSet<ProduitFormationAppellationRome>();
+        }
+        [Key]
+        [Required(AllowEmptyStrings = false)]
+        public int CodeProduitFormation { get; set; }
+
+        public string NiveauFormation { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string LibelleProduitFormation { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(10, ErrorMessage ="Le Libelle Court Formation ne peut pas etre plus long que 10 caracteres")]
+        public string LibelleCourtFormation { get; set; }
+
+        public bool FormationContinue { get; set; }
+        public bool FormationDiplomante { get; set; }
+
+        public virtual ICollection<OffreFormation> OffreFormations { get; set; }
+        public virtual ICollection<ProduitFormationAppellationRome> ProduitFormationAppellationRomes { get; set; }
+    }
+}
