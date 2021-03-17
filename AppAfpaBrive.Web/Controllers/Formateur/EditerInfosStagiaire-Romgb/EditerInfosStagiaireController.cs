5a1c5fe821a52abe5cf496bf3a19da24f6c6f606
louloulabeille <louloulabeille@hotmail.com>
2021-03-17 11:26:31 +0100

    correction des erreurs qui se trouve encore dans la branch develop

--- AppAfpaBrive.Web/Controllers/Formateur/EditerInfosStagiaire-Romgb/EditerInfosStagiaireController.cs
+++ AppAfpaBrive.Web/Controllers/Formateur/EditerInfosStagiaire-Romgb/EditerInfosStagiaireController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.WebPages.Html;
 
 namespace AppAfpaBrive.Web.Controllers.Formateur.EditerInfosStagiaire_Romgb
 {
