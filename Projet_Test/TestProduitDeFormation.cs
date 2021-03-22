9eca3df4298984850f4191a8eef61174cdf18c9a
Besancon Gabriel <besancon.gabriel@hotmail.fr>
2021-03-22 14:19:19 +0100

    ajout de la classe validation services pour les test unitaires et début des tests unitaire pour ProduitFormation

--- Projet_Test/TestProduitDeFormation.cs
+++ Projet_Test/TestProduitDeFormation.cs
@@ -7,21 +7,29 @@ using System.Threading.Tasks;
 using AppAfpaBrive.DAL;
 using AppAfpaBrive.Web.ModelView;
 using AppAfpaBrive.BOL;
+using Microsoft.EntityFrameworkCore;
+using AppAfpaBrive.Web.Controllers.ProduitFormation;
+using Microsoft.AspNetCore.Mvc;
+using ReflectionIT.Mvc.Paging;
 
 namespace Projet_Test
 {
+    [TestFixture]
     public class TestProduitDeFormation
     {
         [Test]
         public void LibelleCourtFormationTropLong()
         {
-            ProduitFormation produit = new ProduitFormation
+            ProduitFormationModelView produit = new ProduitFormationModelView
             {
                 CodeProduitFormation = 6,
-                NiveauFormation= "3",
-                LibelleCourtFormation = "azerazerzee"
+                NiveauFormation="3",
+                LibelleProduitFormation = "azdfazddzdzdzd",
+                LibelleCourtFormation = "ABCDEFSDFZSS"
             };
-            //Assert.IsFalse(produit.IsValid);
+            Assert.IsTrue(ValidationService.ValidateModel(produit).Any(va => 
+            va.MemberNames.Contains("LibelleCourtFormation")
+            && va.ErrorMessage.Contains("Le Libelle Court Formation")));
         }
 
         [Test]
@@ -31,9 +39,109 @@ namespace Projet_Test
             {
                 CodeProduitFormation = 6,
                 NiveauFormation = "3",
-                LibelleCourtFormation = "azerazerzee"
+                LibelleProduitFormation="zdzd",
+                LibelleCourtFormation = "azerazerzc"
             };
-            //Assert.IsTrue(produit.IsValid);
+            Assert.IsFalse(ValidationService.ValidateModel(produit).Any
+                (va => va.MemberNames.Contains("LibelleCourtFormation")
+                && va.ErrorMessage.Contains("Le Libelle Court Formation")));
         }
+        
+        [Test]
+        public void LibelleFormationAbsent()
+        {
+            ProduitFormationModelView produit = new ProduitFormationModelView
+            {
+                CodeProduitFormation = 6,
+                NiveauFormation = "3",
+                LibelleCourtFormation = "azerazerzc"
+            };
+            Assert.IsTrue(ValidationService.ValidateModel(produit).Any
+                (va => va.MemberNames.Contains("LibelleProduitFormation")
+                && va.ErrorMessage.Contains("Le libelle du produit")));
+        }
+        [Test]
+        public void LibelleFormationPresent()
+        {
+            ProduitFormationModelView produit = new ProduitFormationModelView
+            {
+                CodeProduitFormation = 6,
+                NiveauFormation = "3",
+                LibelleProduitFormation = "zdzd",
+                LibelleCourtFormation = "azerazerzc"
+            };
+            Assert.IsFalse(ValidationService.ValidateModel(produit).Any
+                (va => va.MemberNames.Contains("LibelleProduitFormation")
+                && va.ErrorMessage.Contains("Le libelle du produit")));
+        }
+
+        [Test]
+        public void CodeProduitFormationPresent()
+        {
+            ProduitFormationModelView produit = new ProduitFormationModelView
+            {
+                CodeProduitFormation = 6,
+                NiveauFormation = "3",
+                LibelleProduitFormation = "zdzd",
+                LibelleCourtFormation = "azerazerzc"
+            };
+            Assert.IsFalse(ValidationService.ValidateModel(produit).Any
+                (va => va.MemberNames.Contains("CodeProduitFormation")
+                && va.ErrorMessage.Contains("Le code produit formation")));
+        }
+        // dur a tester étant donné que int n'est pas nullable et donc CodeproduitFormation
+        //prend naturellement 0 comme valeur quand non fourni
+        //[Test]
+        //public void CodeProduitFormationAbsent()
+        //{
+        //    ProduitFormationModelView produit = new ProduitFormationModelView
+        //    {
+        //        NiveauFormation = "3",
+        //        LibelleProduitFormation = "zdzd",
+        //        LibelleCourtFormation = "azerazerzc"
+        //    };
+        //    Assert.IsTrue(ValidationService.ValidateModel(produit).Any
+        //        (va => va.MemberNames.Contains("CodeProduitFormation")
+        //        && va.ErrorMessage.Contains("Le code produit formation")));
+        //}
+
+        [Test]
+        [TestCase("concepteur",1, "CodeProduitFormation")]
+        public async Task Test_Creation_De_La_Vue_True(string b, int page, string c)
+        {
+            DbContextOptionsBuilder<AFPANADbContext> builder = new DbContextOptionsBuilder<AFPANADbContext>();
+            builder.UseSqlServer("data source=localhost;initial catalog=AFPANA;integrated security=True;", assembly => assembly.MigrationsAssembly(typeof(AFPANADbContext).Assembly.FullName));
+
+
+            ProduitFormationController controleur = new ProduitFormationController(new AFPANADbContext(builder.Options));
+            var view = await controleur.Index("concepteur",1, "CodeProduitFormation");
+
+            Assert.IsInstanceOf<ViewResult>(view);
+        }
+
+        [Test]
+        [TestCase(4)]
+        public void TestViewEditionValide(int id)
+        {
+            DbContextOptionsBuilder<AFPANADbContext> builder = new DbContextOptionsBuilder<AFPANADbContext>();
+            builder.UseSqlServer("data source=localhost;initial catalog=AFPANA;integrated security=True;", assembly => assembly.MigrationsAssembly(typeof(AFPANADbContext).Assembly.FullName));
+
+            ProduitFormationController controleur = new ProduitFormationController(new AFPANADbContext(builder.Options));
+            var view = controleur.Edit(id);
+            Assert.IsInstanceOf<ViewResult>(view);
+        }
+
+        //[Test]
+        //[TestCase(6)]
+        //public void TestViewDeleteValide(int id)
+        //{
+        //    DbContextOptionsBuilder<AFPANADbContext> builder = new DbContextOptionsBuilder<AFPANADbContext>();
+        //    builder.UseSqlServer("data source=localhost;initial catalog=AFPANA;integrated security=True;", assembly => assembly.MigrationsAssembly(typeof(AFPANADbContext).Assembly.FullName));
+
+        //    ProduitFormationController controleur = new ProduitFormationController(new AFPANADbContext(builder.Options));
+        //    var view = controleur.Delete(id);
+        //    Assert.IsInstanceOf<ViewResult>(view);
+        //}
+
     }
 }
