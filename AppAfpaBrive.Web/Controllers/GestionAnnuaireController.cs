bd95fd9336bf428d74a7fee3e9ca318f8df60a04
Tristan <tristanranchon24@hotmail.fr>
2021-03-29 10:29:41 +0200

    Ajout Controller GestionAnnuaireController et view Index

---
+++ AppAfpaBrive.Web/Controllers/GestionAnnuaireController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.Controllers
+{
+    public class GestionAnnuaireController : Controller
+    {
+        // GET: GerstionAnnuaireController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GerstionAnnuaireController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GerstionAnnuaireController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GerstionAnnuaireController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GerstionAnnuaireController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GerstionAnnuaireController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GerstionAnnuaireController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GerstionAnnuaireController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AppAfpaBrive.Web/ModelView/AnnuaireModelView/StructureModelView.cs
@@ -0,0 +1,38 @@
+﻿using AppAfpaBrive.BOL.AnnuaireSocial;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.ModelView.AnnuaireModelView
+{
+    public class StructureModelView
+    {
+
+        [Required]
+        public string NomStructure { get; set; }
+
+        [Required]
+        public string LigneAdresse1 { get; set; }
+
+        public string LigneAdresse2 { get; set; }
+        public string LigneAdresse3 { get; set; }
+
+        [Required]
+        public string CodePostal { get; set; }
+
+        [Required]
+        public string Ville { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Telephone { get; set; }
+
+
+        public ICollection<LigneAnnuaire> LigneAnnuaires { get; set; }
+        public ICollection<ContactStructure> ContactStructures { get; set; }
+
+
+    }
+}
