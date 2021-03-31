1252c85a77ff289229d60d6675409309113b7b8b
louloulabeille <louloulabeille@hotmail.com>
2021-03-31 14:47:57 +0200

    navigation

---
+++ AppAfpaBrive.Web/Controllers/NavigationStagiaireController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.Controllers
+{
+    public class NavigationStagiaireController : Controller
+    {
+        /// <summary>
+        /// action qui va afficher dans une seule fenetre le menu pour le bénéficiare
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Index(string matricule)
+        {
+            if (string.IsNullOrWhiteSpace(matricule))
+                return NotFound();
+             
+
+            return View();
+        }
+    }
+}
---
+++ AppAfpaBrive.Web/ModelView/Navigation/BeneficiaireNavigationModelView.cs
@@ -0,0 +1,16 @@
+﻿using AppAfpaBrive.BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppAfpaBrive.Web.ModelView.Navigation
+{
+    public class BeneficiaireNavigationModelView
+    {
+        public string MatriculeBeneficiaire { get; set; }
+        public string NomBeneficiaire { get; set; }
+        public string PrenomBeneficiaire { get; set; }
+
+    }
+}
