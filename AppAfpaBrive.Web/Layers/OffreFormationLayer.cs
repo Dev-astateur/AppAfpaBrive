969ca5b01d0b5bfe5a712dfb3d4994e7c2050dde
louloulabeille <louloulabeille@hotmail.com>
2021-04-01 11:40:39 +0200

    rajout des role dans le secret - et passez les migrations du dbcontext entity

--- AppAfpaBrive.Web/Layers/OffreFormationLayer.cs
+++ AppAfpaBrive.Web/Layers/OffreFormationLayer.cs
@@ -1,8 +1,8 @@
 ﻿using AppAfpaBrive.BOL;
 using AppAfpaBrive.DAL;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
